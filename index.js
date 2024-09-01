62aaf88a82a4ee111f4b4574623bce44c873bf53
adm-2024 <olinebutwhtap@gmail.com>
2024-09-01 04:39:54 -0500

    vamos

--- index.js
+++ index.js
@@ -21,17 +21,7 @@ app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
-// Obtener __dirname en un módulo ES
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-
-const publicPath = path.join(__dirname, 'src', 'public');
-
-// Middleware para servir archivos estáticos
-app.use('/public', express.static(publicPath));
-
+ 
 
 
 
@@ -44,21 +34,11 @@ app.use('/api/auth', authRoutes);
 app.use('/api/getadmin', getAdminRoutes);
 app.use('/api/colector', colectorRoutes);
 
-
-// Middleware para manejar rutas no encontradas
-app.use((req, res, next) => {
-  res.status(404).json({ message: 'Ruta no encontrada' });
-});
-
-// Middleware para manejo de errores
-app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).json({ message: 'Error interno del servidor' });
-});
-
+ 
 // Iniciar el servidor
 const PORT = process.env.PORT || 4001;
 
 app.listen(PORT, () => {
   console.log(`Servidor corriendo en el puerto ${PORT}`);
 });
+ 
